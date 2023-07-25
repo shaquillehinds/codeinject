f806125f766cdb2d98c922dd46001c2b8751a8fb
Shaquille <shaqdulove@hotmail.com>
2023-07-25 17:50:14 -0400

    created reducerState.return.statements.ts template

---
+++ src/templates/statements/reducerState.return.statement.ts
@@ -0,0 +1,15 @@
+import { JSCodeshift } from "jscodeshift";
+import objToExp from "../../pipeline/parsers/objToExp.parser";
+
+export default function reducerStateReturnStatement(
+  jcs: JSCodeshift,
+  payload: { [key: string]: any }
+) {
+  const value = objToExp(jcs, payload);
+  const objExp = jcs.objectExpression([
+    jcs.spreadElement(jcs.identifier("state")),
+  ]);
+  for (let prop in value.properties)
+    objExp.properties.push(value.properties[prop]);
+  return jcs.returnStatement(objExp);
+}
