9d525ac2ec101e57dc7346543cd4333d72e567d6
Shaquille <shaqdulove@hotmail.com>
2023-07-27 13:55:34 -0400

    Added  ts aliases, Logger.ts and vscode debug logger snippet

---
+++ src/utils/Logger.ts
@@ -0,0 +1,16 @@
+import chalk from "chalk";
+
+export function Logger(input: any[], source = "Logger") {
+  if (input[0] === "warn") {
+    console.warn(chalk.yellow(source + ": ", ...input.slice(1)));
+  } else if (input[0] === "error") {
+    console.error(chalk.redBright(source + ": ", ...input.slice(1)));
+  } else {
+    console.log(source + ": ", ...input);
+  }
+}
+export function DebugLogger(source: string) {
+  return (...args: any[]) => Logger(args, source + "/");
+}
+
+export default Logger;
