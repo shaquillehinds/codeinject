33df284a9e46510f3312e9421493152e73ac8295
Shaquille <shaqdulove@hotmail.com>
2023-07-27 13:56:02 -0400

    Removed deleted commands from commands index

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,17 +1,3 @@
-import addCommand from "./add.command";
 import newCommand from "./new.command";
-import listCommand from "./list.command";
-import findCommand from "./find.command";
-import updateCommand from "./update.command";
-import deleteCommand from "./delete.command";
-import findByIdCommand from "./findById.command";
 
-export {
-  addCommand,
-  newCommand,
-  listCommand,
-  findCommand,
-  updateCommand,
-  deleteCommand,
-  findByIdCommand,
-};
+export { newCommand };
