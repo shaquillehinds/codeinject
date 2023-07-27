3edab703f80e03d414871e662851dee7d41b5f4a
Shaquille <shaqdulove@hotmail.com>
2023-07-27 19:42:27 -0400

    updated new.command.ts

--- src/commands/new.command.ts
+++ src/commands/new.command.ts
@@ -3,6 +3,8 @@ import { readFileSync, writeFileSync } from "fs";
 import inquirer from "inquirer";
 import newFinderCommandPipeline from "./pipelines/newFinder.command.pipeline";
 import newStageCommandPipeline from "./pipelines/newStage.command.pipeline";
+import chalk from "chalk";
+import { FinderTypeE } from "@src/@types/finder.enums";
 
 type NewType = "stage" | "finder";
 
@@ -12,8 +14,12 @@ export default function newCommand(program: Command) {
     .description("Creates a new stage or finder")
     // dash syntax resolves to camel case --first-name=firstName
     .option("-n --name <string>", "Name of stage or finder")
+    .option(
+      "-f --finder <string>",
+      "Name of finder you want to use for the stage"
+    )
     .option("-c --collection <string>", "Name of stage collection")
-    .action(async (type, { name, collection }) => {
+    .action(async (type, { name, collection, finder }) => {
       let newType = type as NewType;
       if (!type) {
         newType = (
@@ -47,16 +53,40 @@ export default function newCommand(program: Command) {
           "utf-8"
         );
       } else if (newType === "stage") {
-        let col = collection;
-        if (!col)
-          col = (
+        if (!collection)
+          collection = (
             await inquirer.prompt({
               name: "col",
               type: "input",
-              message: "Name of collection this stage will be using.",
+              message: "Name of collection this stage will be using?",
             })
           ).col;
-        await newStageCommandPipeline(name, col);
+        if (!finder)
+          finder = (
+            await inquirer.prompt({
+              name: "finder",
+              type: "input",
+              message: "Name of finder this stage will be using?",
+            })
+          ).finder;
+        if (finder && !FinderTypeE[finder as FinderType]) {
+          console.info(
+            chalk.cyanBright(
+              "This finder does not exist, so it will be created"
+            )
+          );
+          await newFinderCommandPipeline(finder);
+          const finderL = finder[0].toLowerCase() + finder.slice(1);
+          writeFileSync(
+            `src/pipeline/finders/${finderL}.finder.ts`,
+            readFileSync(`src/templates/files/finder.template`, "utf-8")
+              .replaceAll("{!template}", finder)
+              .replaceAll("{template}", finderL),
+            "utf-8"
+          );
+        }
+
+        await newStageCommandPipeline(name, collection);
         writeFileSync(
           `src/pipeline/stages/${nameL}.inject.stage.ts`,
           readFileSync(`src/templates/files/stage.template`, "utf-8")
