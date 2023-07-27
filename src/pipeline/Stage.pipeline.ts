6cde911e8f120f2cb0e1756aae5d9a12e3b937d2
Shaquille <shaqdulove@hotmail.com>
2023-07-27 19:12:29 -0400

    Added injection methods to Stage.pipleline.ts

--- src/pipeline/Stage.pipeline.ts
+++ src/pipeline/Stage.pipeline.ts
@@ -2,6 +2,8 @@ import { Collection, JSCodeshift } from "jscodeshift";
 import { readFileSync, writeFileSync } from "fs";
 import * as prettier from "prettier";
 import chalk from "chalk";
+import s from "./stages";
+import f from "./finders";
 
 export default class StagePipeline {
   protected ast?: Collection;
@@ -56,6 +58,111 @@ export default class StagePipeline {
       }
     }
     this.asts = [];
+    return this;
+  }
+
+  public injectArrayElement(
+    stageOptions: StageOptions<"arrayElement">,
+    finderOptions: FinderOptions<"variableArray">
+  ) {
+    if (!this.ast) this.parse();
+    stageOptions.col = f.arrayVariableFinder(this.j, this.ast!, finderOptions);
+    s.injectArrayElementStage(this.j, this.ast!, stageOptions);
+    return this;
+  }
+
+  public injectTSEnumMember(
+    stageOptions: StageOptions<"tsEnumMember">,
+    finderOptions: FinderOptions<"tsEnum">
+  ) {
+    if (!this.ast) this.parse();
+    stageOptions.col = f.tsEnumFinder(this.j, this.ast!, finderOptions);
+    s.injectTSEnumMemberStage(this.j, this.ast!, stageOptions);
+    return this;
+  }
+
+  public injectTSTypeAliasConditional(
+    stageOptions: StageOptions<"tsTypeAliasConditional">,
+    finderOptions: FinderOptions<"tsTypeAlias">
+  ) {
+    if (!this.ast) this.parse();
+    stageOptions.col = f.tsTypeAliasFinder(this.j, this.ast!, finderOptions);
+    s.injectTSTypeAliasConditionalStage(this.j, this.ast!, stageOptions);
+    return this;
+  }
+
+  public injectProperty(
+    stageOptions: StageOptions<"property">,
+    finderOptions: FinderOptions<"variableObject">
+  ) {
+    if (!this.ast) this.parse();
+    stageOptions.col = f.objectVariableFinder(this.j, this.ast!, finderOptions);
+    s.injectPropertyStage(this.j, this.ast!, stageOptions);
+    return this;
+  }
+
+  public injectSwitchCase(
+    stageOptions: StageOptions<"case">,
+    finderOptions: FinderOptions<"switch">
+  ) {
+    if (!this.ast) this.parse();
+    stageOptions.col = f.switchFinder(this.j, this.ast!, finderOptions);
+    s.injectSwitchCaseStage(this.j, this.ast!, stageOptions);
+    return this;
+  }
+
+  public injectTSTypeAlias(
+    stageOptions: StageOptions<"tsTypeAlias">,
+    finderOptions: FinderOptions<"tsTypeAlias">
+  ) {
+    if (!this.ast) this.parse();
+    stageOptions.col = f.tsTypeAliasFinder(this.j, this.ast!, finderOptions);
+    s.injectTSTypeAliasStage(this.j, this.ast!, stageOptions);
+    return this;
+  }
+
+  public injectTSTypeLiteral(
+    stageOptions: StageOptions<"tsTypeLiteral">,
+    finderOptions: FinderOptions<"tsTypeLiteral">
+  ) {
+    if (!this.ast) this.parse();
+    stageOptions.col = f.tsTypeLiteralFinder(this.j, this.ast!, finderOptions);
+    s.injectTSTypeLiteralStage(this.j, this.ast!, stageOptions);
+    return this;
+  }
+
+  public injectTSInterfaceBody(
+    stageOptions: StageOptions<"tsInterfaceBody">,
+    finderOptions: FinderOptions<"tsInterfaceBody">
+  ) {
+    if (!this.ast) this.parse();
+    stageOptions.col = f.tsInterfaceBodyFinder(
+      this.j,
+      this.ast!,
+      finderOptions
+    );
+    s.injectTSInterfaceBodyStage(this.j, this.ast!, stageOptions);
+    return this;
+  }
+
+  public injectNamedExport(stageOptions: StageOptions<"namedExport">) {
+    if (!this.ast) this.parse();
+    stageOptions.col = f.exportFinder(this.j, this.ast!);
+    s.injectNamedExportStage(this.j, this.ast!, stageOptions);
+    return this;
+  }
+
+  public injectImport(stageOptions: StageOptions<"import">) {
+    if (!this.ast) this.parse();
+    stageOptions.col = f.importFinder(this.j, this.ast!);
+    s.injectImportStage(this.j, this.ast!, stageOptions);
+    return this;
+  }
+
+  public injectStringTemplate(stageOptions: StageOptions<"stringTemplate">) {
+    if (!this.ast) this.parse();
+    stageOptions.col = f.programFinder(this.j, this.ast!);
+    s.injectStringTemplateStage(this.j, this.ast!, stageOptions);
     return this;
   }
 }
