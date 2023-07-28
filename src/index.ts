16c3aa6f1e2b050504fc61aa75c193286a9ef9f3
Shaquille <shaqdulove@hotmail.com>
2023-07-28 13:24:33 -0400

    Removed experimental specifier resolution since tsc-alias can resolve js

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node --experimental-specifier-resolution=node
-
 import InjectionPipeline from "./pipeline/Injection.pipeline";
 import reducerStateReturnStatement from "./templates/statements/reducerState.return.statement";
 
