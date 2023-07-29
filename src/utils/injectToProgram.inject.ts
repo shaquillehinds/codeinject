f9722821480f4699d0d7bebbbbc5d915c8fd93bc
Shaquille <shaqdulove@hotmail.com>
2023-07-29 15:04:26 -0400

    Added injectToProgram and stringToStatements utilities

---
+++ src/utils/injectToProgram.inject.ts
@@ -0,0 +1,21 @@
+import { StatementKind } from "ast-types/gen/kinds";
+import programFinder from "@src/pipeline/finders/program.finder";
+import jcs, { Collection } from "jscodeshift";
+
+export default function injectToProgram(
+  ast: Collection,
+  injections: {
+    statement: StatementKind;
+    injectionLine?: "last" | "first";
+  }[]
+) {
+  const programCol = programFinder(jcs, ast);
+
+  programCol.forEach(path => {
+    injections.forEach(({ statement, injectionLine }) => {
+      injectionLine === "first"
+        ? path.node.body.unshift(statement)
+        : path.node.body.push(statement);
+    });
+  });
+}
---
+++ src/utils/stringToStatements.inject.ts
@@ -0,0 +1,29 @@
+import programFinder from "@src/pipeline/finders/program.finder";
+import jcs from "jscodeshift";
+import { StatementKind } from "ast-types/gen/kinds";
+
+/**
+ *
+ * @param string - Provide code program in string format
+ * @example
+ * ```
+ * `
+ *  const helloResponse = "world";
+ *
+ *  function hello(){
+ *    console.log(helloResponse);
+ *  }
+ * `
+ * ```
+ */
+export default function stringToStatements(str: string) {
+  const ast = jcs.withParser("tsx")(str);
+
+  const programCol = programFinder(jcs, ast);
+  let statements: StatementKind[] = [];
+  programCol
+    .paths()[0]
+    .node.body.forEach(statement => statements.push(statement));
+
+  return statements;
+}
