5f7023648a8adcca5702430d31b9f2cf4c742151
Shaquille <shaqdulove@hotmail.com>
2023-07-30 01:23:42 -0400

    Updated jest and ts config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,12 @@ export default {
   verbose: true,
   preset: "ts-jest/presets/js-with-babel",
   testEnvironment: "node",
-  transformIgnorePatterns: ["node_modules/(?!chalk/.*)"]
+  transformIgnorePatterns: ["node_modules/(?!chalk/.*)"],
+  moduleNameMapper: {
+    "^@src/(.*)$": "<rootDir>/src/$1",
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+    "^@commands/(.*)$": "<rootDir>/src/commands/$1",
+    "^@templates/(.*)$": "<rootDir>/src/templates/$1",
+    "^@pipeline/(.*)$": "<rootDir>/src/pipeline/$1"
+  }
 };
